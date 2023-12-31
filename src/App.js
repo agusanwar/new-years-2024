1d9a3df2e606afbbf5d71ad3fb6add753d7250f4
agusanwar <agusanwar@gmail.com>
2023-12-31 16:29:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from "react-particles";
+import {loadFireworksPreset} from "tsparticles-preset-fireworks"
+import { Typewriter } from "react-simple-typewriter";
+import { useState } from "react";
+import Countdown from "react-countdown";
+
 
 function App() {
+  const [newYearsMessage, setNewYearsMessage] = useState(["Thank You 2023...!"])
+  // init particels
+  const particelIntilialization = async(engine) =>{
+    await loadFireworksPreset(engine)
+  }
+
+  // logic time
+  function timeLeft(){
+    const newYearDate = new Date("January 1, 2024 00:00:00:00").getTime()
+    const nowDate = new Date().getTime()
+    const remainingTime = newYearDate - nowDate
+    return remainingTime
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles 
+        init={particelIntilialization}
+        options={{ preset: "fireworks" }}
+      />
+      {/* react typewriter */}
+      <div className="flex flex-col justify-center items-center min-h-screen gap-4">
+         {/* z-50 for item up screens */}
+        <span className="text-white text-4xl font-bold px-4 z-50">
+          <Typewriter 
+            words={newYearsMessage}
+            loop={false}
+            cursorStyle={"_"}
+            cursor
+            typeSpeed={30}
+            deleteSpeed={20}
+         />
+        </span>
+        <div className="z-50 text-white font-bold text-2xl">
+          <Countdown 
+            date={Date.now() + timeLeft()} 
+            onComplete={() => setNewYearsMessage([
+              "Selamat", "Tahun", "Baru", "HNY2024"
+            ])}
+            />
+        </div>
+      </div>
+    </>
   );
 }
 
